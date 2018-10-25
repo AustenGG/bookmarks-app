b45b404a8aa3ca0f4df2ceb1c0c9b84d0dbbc720
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-10-25 10:46:22 +0100

    moving onto step10

--- spec/features/bookmark_spec.rb
+++ spec/features/bookmark_spec.rb
@@ -1,12 +1,21 @@
 require 'bookmark'
 
 describe Bookmark do
-  describe '.all' do
-    it 'returns all bookmarks' do
-      bookmarks = Bookmark.all
-      expect(bookmarks).to include "http://www.askjeeves.com"
-      expect(bookmarks).to include  "http://www.twitter.com"
-      expect(bookmarks).to include "http://www.google.com"
-    end
+
+describe '.all' do
+  it 'returns a list of bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES (1,'http://www.askjeeves.com');")
+    connection.exec("INSERT INTO bookmarks VALUES (2,'http://www.twitter.com');")
+    connection.exec("INSERT INTO bookmarks VALUES (3,'http://www.google.com');")
+
+    bookmarks = Bookmark.all
+
+    expect(bookmarks).to include('http://www.askjeeves.com')
+    expect(bookmarks).to include('http://www.twitter.com')
+    expect(bookmarks).to include('http://www.google.com')
   end
+end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -4,9 +4,17 @@ feature 'Viewing bookmarks' do
     expect(page).to have_content "Bookmark Manager"
   end
 end
+require 'pg'
 
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.askjeeves.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.twitter.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/bookmarks')
 
     expect(page).to have_content "http://www.askjeeves.com"
